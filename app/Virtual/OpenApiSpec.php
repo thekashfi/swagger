22b7227541cc97796c1dbb410fe83758968c935c
thekashfi <alikashfi12345@gmail.com>
2023-01-27 15:05:05 +0330

    first generated swagger docs(wrong data - messed up code)

---
+++ app/Virtual/OpenApiSpec.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+/**
+* @OA\Tag(
+*     name="user",
+*     description="User related operations"
+* )
+* @OA\Info(
+*     version="1.0",
+*     title="Example API",
+*     description="Example info",
+*     @OA\Contact(name="Swagger API Team")
+* )
+* @OA\Server(
+*     url="https://example.localhost",
+*     description="API server"
+* )
+*/
+class OpenApiSpec
+{
+}
